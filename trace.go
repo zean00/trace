e59f7399a0acea371265b5ceb0110b37edbf1cf0
drymonsoon <tirabassigiacomo@gmail.com>
2017-07-24 17:46:43 +0200

    refactored

--- trace.go
+++ trace.go
@@ -1,15 +1,10 @@
 package trace
 
 import (
-	"context"
-	"fmt"
 	"io"
 	"log"
 	"os"
 
-	"github.com/micro/go-micro/metadata"
-	"github.com/micro/go-micro/server"
-	opentracing "github.com/opentracing/opentracing-go"
 	jaeger "github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
@@ -56,34 +51,4 @@ func Initialization(name string) (io.Closer, error) {
 		return nil, err
 	}
 	return closer, nil
-}
-
-//MicroSubscriber is usefull to track those calls made from subscription
-func MicroSubscriber() server.SubscriberWrapper {
-	return func(next server.SubscriberFunc) server.SubscriberFunc {
-		return func(ctx context.Context, msg server.Publication) error {
-			md, _ := metadata.FromContext(ctx)
-			var sp opentracing.Span
-			tr := opentracing.GlobalTracer()
-			name := "SUBS " + msg.Topic() + " " + fmt.Sprint(msg.Message())
-			wireContext, err := tr.Extract(opentracing.TextMap, opentracing.TextMapCarrier(md))
-			if err != nil {
-				sp = tr.StartSpan(name)
-			} else {
-				sp = tr.StartSpan(name, opentracing.FollowsFrom(wireContext))
-			}
-			if err := sp.Tracer().Inject(sp.Context(), opentracing.TextMap, opentracing.TextMapCarrier(md)); err != nil {
-				return err
-			}
-			ctx = metadata.NewContext(ctx, md)
-			err = next(ctx, msg)
-			if err != nil {
-				return err
-			}
-
-			//this happens after the subsciber handler has finish
-			sp.Finish()
-			return nil
-		}
-	}
 }
